afc7006ab45326db102fe1cf31fd884c1c9e5834
Márcio Xavier <43187704+Marcio-Xavier@users.noreply.github.com>
2019-06-12 16:24:05 -0300

    Update obterPreco.js

--- colorcom/colorcom/Scripts/Geral/ItemPedido/obterPreco.js
+++ colorcom/colorcom/Scripts/Geral/ItemPedido/obterPreco.js
@@ -1,4 +1,4 @@
-﻿var obterPreco = () => {
+var obterPreco = () => {
     var item = document.getElementById("DropDownItem");
     try {
         $.getJSON(ItemPedidoUrl.itemSelecionado + "?itemID=" + item.value, function (result) {
@@ -13,16 +13,59 @@
 }
 
 var atualizarPreco = (result) => {
-    var preco = document.getElementById("TxtBoxPreco");
-    preco.value = result[0].Preco;
-    calcularValor();
+    //var precoOriginal = result[0].Preco;
+    var precoOriginal = 60;//Apagar
+    var qtd = parseInt(document.getElementById("TxtBoxQuantidade").value);
+    var total = qtd * precoOriginal;
+    $('#TxtBoxPreco').val(total);
 }
 
-var calcularValor = () => {
-    //var total = 0;
-    var preco = parseInt(document.getElementById("TxtBoxPreco").value);
-    var qtd = parseInt(document.getElementById("TxtBoxQuantidade").value);
-    var total = preco * qtd;
-    console.log(preco + "*" + qtd + "=" + total);
-    $('#TxtBoxPreco').val(total);
+var atualizarSubtotal = (btn) => {
+    var linha = $(btn).closest("tr");
+    var precoVenda = parseFloat(linha.find("#tdPreco").text());
+    var qtdAtual = parseInt(linha.find("#tdQtd").text());
+    var subTotal = parseFloat(linha.find("#tdSubTotal").text());
+    
+    if (btn.id == "btnMais"){
+        if (qtdAtual < 10){
+            qtdAtual ++;
+            subTotal = qtdAtual * precoVenda;
+            linha.find("#tdSubTotal").text(subTotal);
+            linha.find("#tdQtd").text(qtdAtual);
+        }
+    }else{
+        if (qtdAtual > 1){
+            qtdAtual --;
+            subTotal = qtdAtual * precoVenda;
+            linha.find("#tdSubTotal").text(subTotal);
+            linha.find("#tdQtd").text(qtdAtual);
+        }
+    }
+    atualizarTotal();
+    //var precoOriginal = result[0].Preco;
+    // var precoOriginal = 60;//Apagar
+    // var qtd = parseInt(document.getElementById("TxtBoxQtdRow").value);
+    // var total = qtd * precoOriginal;
+    // $('#TxtBoxPreco').val(total);    
+}
+
+var atualizarTotal = () => {
+    var total = 0;
+    var tabela = document.getElementById("tabelaItens");
+    for(var i = 1; i < tabela.rows.length - 1; i++)
+    {
+        total = total + parseFloat(tabela.rows[i].cells[4].innerHTML);
+    }
+    $('#tdTotal').text(total);
+}
+
+var adicionarItem = (item) => {
+    var row = '<tr> <th scope="row">1</th> <td>Pincel</td> <td id="tdPreco">20</td><td id="tdQtd" class="fit-column">1</td><td id="tdSubTotal" class=".calc-sub">20</td> <td class="fit-column"> <button type="button" class="btn btn-success btn-sm" id="btnMais" onclick="atualizarSubtotal(this)"><b>+</b></button> <button type="button" class="btn btn-warning btn-sm" id="btnMenos" onclick="atualizarSubtotal(this)"><b>-</b></button> <button type="button" class="btn btn-outline-danger btn-sm" onclick="removerItem(this)">Remover</button> </td> </tr>';
+    $('#tabelaItens tr:last').before(row);
+}
+
+var removerItem = (item) => {
+	var tr = $(item).closest('tr');	
+    tr.remove();
+    return false;
 }
